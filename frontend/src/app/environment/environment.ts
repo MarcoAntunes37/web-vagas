f6fea87c756290a164873b72f0c2dcdae142f318
Marco Antunes <marcoantunesdev@gmail.com>
2025-08-08 09:12:00 -0300

    remove dynamic generated file from project

--- frontend/src/app/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  "production": false,
-  "userPreferencesApiUrl": "http://localhost:9000/api/v1/preferences/jsearch/user",
-  "checkoutSessionApiUrl": "http://localhost:9000/api/v1/checkout-session/create-checkout-session"
-};
