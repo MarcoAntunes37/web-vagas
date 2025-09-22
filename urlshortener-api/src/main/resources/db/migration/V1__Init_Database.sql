2ab9fd39116615468df451f6a6375b5eb38d744c
Marco Antunes <marcoantunesdev@gmail.com>
2025-09-22 09:27:21 -0300

    fix: fix script

--- urlshortener-api/src/main/resources/db/migration/V1__Init_Database.sql
+++ urlshortener-api/src/main/resources/db/migration/V1__Init_Database.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS short_url
 (
     id BIGSERIAL NOT NULL,
     code character varying(20) COLLATE pg_catalog."default",
-    original_url character VARCHAR(2000) COLLATE pg_catalog."default",
+    original_url character varying(2000) COLLATE pg_catalog."default",
     created_at timestamp(6) with time zone,
     CONSTRAINT short_url_pkey PRIMARY KEY (id),
     CONSTRAINT code_ukey UNIQUE (code)
