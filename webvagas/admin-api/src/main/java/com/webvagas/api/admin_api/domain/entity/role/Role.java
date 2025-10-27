92a5520de7347ccd99a4f8a9a50e48b2cd081897
Marco Antunes <marcoantunesdev@gmail.com>
2025-10-27 11:27:21 -0300

    fix: remove lombok and add methods to retrieve data and delegation from valueObject

--- webvagas/admin-api/src/main/java/com/webvagas/api/admin_api/domain/entity/role/Role.java
+++ webvagas/admin-api/src/main/java/com/webvagas/api/admin_api/domain/entity/role/Role.java
@@ -3,14 +3,44 @@ package com.webvagas.api.admin_api.domain.entity.role;
 import com.webvagas.api.admin_api.domain.value_object.RoleAttributes;
 import com.webvagas.api.admin_api.domain.value_object.RoleDetails;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class Role {
     private RoleDetails details;
     private RoleAttributes attributes;
+
+    public Role(RoleDetails details, RoleAttributes attributes) {
+        this.details = details;
+        this.attributes = attributes;
+    }
+
+    public RoleDetails getDetails() {
+        return details;
+    }
+
+    public RoleAttributes getAttributes() {
+        return attributes;
+    }
+
+    public String getRoleDetailsId() {
+        return details.getId();
+    }
+
+    public String getRoleDetailsName() {
+        return details.getName();
+    }
+
+    public String getRoleDetailsDescription() {
+        return details.getDescription();
+    }
+
+    public Boolean getRoleAttributesComposite() {
+        return attributes.isComposite();
+    }
+
+    public Boolean getRoleAttributesClientRole() {
+        return attributes.isClientRole();
+    }
+
+    public String getRoleAttributesContainerId() {
+        return attributes.getContainerId();
+    }
 }
